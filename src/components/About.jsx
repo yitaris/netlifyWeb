1bfd05e1765843d6c26e1fcc5729faa4d3d12cfa
Yiğit Bayır <yitaris>
2024-10-17 13:20:31 +0300

    supernatural

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ const ServiceCard = ({ index, title, icon }) => (
   <div className='xs:w-[250px] w-full'>
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className='w-full rounded-[20px] shadow-card'
+      className='w-full rounded-[20px] '
     >
       <div
         className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
