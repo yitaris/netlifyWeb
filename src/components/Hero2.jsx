6f0f67640d1ac113fdbd787394b63db923346e74
Yiğit Bayır <yitaris>
2024-10-20 03:26:24 +0300

    nunun

--- src/components/Hero2.jsx
+++ src/components/Hero2.jsx
@@ -78,7 +78,9 @@ const Hero2 = () => {
                                     <Button Title={'Projelerim'} isToggle={isToggled} Delay={.1} />
                                     <Button Title={'Yeteneklerim'} isToggle={isToggled} Delay={.2} />
                                 </div>
+                                <div className="flex flex-col gap-7">
                                     <Button Title={'Referanslarım'} isToggle={isToggled} Delay={.3} />
+                                </div>
                             </div>
                             <div className="flex flex-row gap-7">
                                     <Button Title={'İletişime Geç'} isToggle={isToggled} Delay={.4} />
